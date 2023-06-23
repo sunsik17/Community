5de66cf09c13ebfdf5ab6a200e5ee9bec646d946
sunsik <sunsig21c@gmail.com>
2023-06-23 19:45:14 +0900

    [refactor/multi-module_build] mailgun-client 모듈을 module-client의 하위 모듈로 바꿈

--- Community/module-admin/src/main/java/com/zerobase/admin/AdminApplication.java
+++ Community/module-admin/src/main/java/com/zerobase/admin/AdminApplication.java
@@ -2,8 +2,14 @@ package com.zerobase.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.zerobase")
+@EntityScan(basePackages = "com.zerobase.common")
+@EnableJpaRepositories(basePackages = "com.zerobase.common")
+@EnableJpaAuditing
 public class AdminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
